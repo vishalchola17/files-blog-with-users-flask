09258359b2d23b7353a11836024cfcf02705fbf1
vishalchola17 <vishalchola121@gmail.com>
2024-01-09 19:35:24 +0530

    commit

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 # Import your forms from the forms.py
 from forms import CreatePostForm, Register, Login, CommentForm
+import os
 
-
+os.environ["FLASK"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 '''
 Make sure the required packages are installed: 
 Open the Terminal in PyCharm (bottom left). 
@@ -26,7 +27,8 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+#app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'D
+app.config['SECRET_KEY'] = os.environ.get("FLASK")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -43,7 +45,8 @@ def admin_only(fun):
     return wrapper
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy()
 db.init_app(app)
 
